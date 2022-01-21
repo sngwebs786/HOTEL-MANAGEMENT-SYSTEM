7932b9773a18b72fc94a63451ef488cdcc6680db
sngwebs786 <sngwebs786@gmail.com>
2022-01-21 11:39:09 +0500

    Work Accomplish

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -6,16 +6,15 @@ import { getDatabase } from "firebase/database";
 
 import * as firebase from "firebase/app";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBW25WEA5V38hENOWASvpaceQY5A1M5diU",
-    authDomain: "hotelmanagementhackathon.firebaseapp.com",
-    projectId: "hotelmanagementhackathon",
-    storageBucket: "hotelmanagementhackathon.appspot.com",
-    messagingSenderId: "79931828304",
-    appId: "1:79931828304:web:b553052a00965f9c420955",
-    measurementId: "G-LBW2THGEZB"
-  };
+  apiKey: "AIzaSyDexbqmHK7b1hoP9N-Tsal65I1U7VFjnqI",
+  authDomain: "hotel-management-system-14e06.firebaseapp.com",
+  projectId: "hotel-management-system-14e06",
+  storageBucket: "hotel-management-system-14e06.appspot.com",
+  messagingSenderId: "379350889090",
+  appId: "1:379350889090:web:eb0726c8425fa115547a9b",
+  measurementId: "G-WTY728EGSE"
+};
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
@@ -28,3 +27,50 @@ const firebaseConfig = {
   export {db,auth,app,fire}
 
   // export default fire;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDexbqmHK7b1hoP9N-Tsal65I1U7VFjnqI",
+//   authDomain: "hotel-management-system-14e06.firebaseapp.com",
+//   projectId: "hotel-management-system-14e06",
+//   storageBucket: "hotel-management-system-14e06.appspot.com",
+//   messagingSenderId: "379350889090",
+//   appId: "1:379350889090:web:eb0726c8425fa115547a9b",
+//   measurementId: "G-WTY728EGSE"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
